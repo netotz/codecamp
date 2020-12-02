880cb38df4ca43ed0816d3b4234c290fdd7fc332
netotz <neto.otz@hotmail.com>
2020-12-01 20:20:47 -0600

    Complete AoC day 1

---
+++ advent_of_code/2020/day1.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+from math import prod
+
+def solve(entries, repeat):
+    for comb in combinations(entries, repeat):
+        if sum(comb) == 2020:
+            return prod(comb)
+
+def get_input():
+    with open('input1.txt') as inputfile:
+        return map(int, inputfile.read().splitlines())
+
+answer1 = solve(get_input(), 2)
+answer2 = solve(get_input(), 3)
+
+def test():
+    sample = [1721, 979, 366, 299, 675, 1456]
+    assert solve(sample, 2) == 514579
+    assert solve(sample, 3) == 241861950
