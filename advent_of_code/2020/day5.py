e3a23b227f7974e443cdbfc92898f4457dfc837f
netotz <neto.otz@hotmail.com>
2020-12-05 14:40:07 -0600

    Add refactored and better solution

--- advent_of_code/2020/day5.py
+++ advent_of_code/2020/day5.py
@@ -5,6 +5,9 @@ def get_half_diff(lower, upper):
     return (upper - lower) / 2
 
 def get_seat_id(seat):
+    '''
+    Used to solve the puzzle.
+    '''
     bottomrow, toprow = 0, 127
     for r in seat[:7]:
         delta = get_half_diff(bottomrow, toprow)
@@ -23,6 +26,25 @@ def get_seat_id(seat):
 
     return (int(toprow) * 8) + int(right)
 
+def ref_get_seat_id(seat):
+    '''
+    Refactored, faster, and better solution.
+    This is my understanding:
+
+    Every seat ID is determined by a unique string of FBLR.
+    There are 128 rows (2 ** 7) and the first 7 characters specify the row.
+    There are 8 columns (2 ** 3) and the last 3 characters specify the column.
+    There are 1024 total seats (2 ** 10) and the string length is 10.
+    If F and L are replaced with 0, because they represent the lower halfs,
+    and B and R with 1, because they represent the upper halfs,
+    then each seat ID is given by one of the binary numbers from 0 to 1023.
+    That's why the formula is (8 * row) + column.
+    '''
+    return int(
+        seat.translate(seat.maketrans('FBLR', '0101')),
+        base=2
+    )
+
 def get_max_id(passes):
     return max(get_seat_id(s) for s in passes)
 
@@ -39,9 +61,13 @@ answer2 = get_missing_id(input5)
 def test():
     sample = 'FBFBBFFRLR'
     assert get_seat_id(sample) == 357
+    assert ref_get_seat_id(sample) == 357
     sample = 'BFFFBBFRRR'
     assert get_seat_id(sample) == 567
+    assert ref_get_seat_id(sample) == 567
     sample = 'FFFBBBFRRR'
     assert get_seat_id(sample) == 119
+    assert ref_get_seat_id(sample) == 119
     sample = 'BBFFBBFRLL'
     assert get_seat_id(sample) == 820
+    assert ref_get_seat_id(sample) == 820
