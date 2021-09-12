df7283e0e7eb52cd79ead601cc98b493df2f9ff5
netotz <neto.otz@hotmail.com>
2021-09-11 19:05:25 -0500

    Add alternative solution

--- leetcode/2sum.py
+++ leetcode/2sum.py
@@ -13,14 +13,47 @@ def two_sum(nums: List[int], target: int) -> List[int]:
                 return [i, i_dif]
 
 
+def two_sum_reverse(nums: List[int], target: int) -> List[int]:
+    '''
+    `.pop()` now takes O(1) and there's no need to use `in` operator,
+    but I don't like how the try block looks
+    '''
+    i = len(nums) - 1
+    while i >= 0:
+        n = nums[-1]
+        nums.pop()
+        try:
+            i_dif = nums.index(target - n)
+            if i != i_dif:
+                return [i_dif, i]
+        except ValueError:
+            pass
+        i -= 1
+
+
+def with_buffer(nums: List[int], target: int) -> List[int]:
+    past = {}
+    for i, num in enumerate(nums):
+        if (dif := target - num) in past:
+            return [past[dif], i]
+        past[num] = i
+
+
 @pytest.mark.parametrize(
-    'nums, target, indexes', [
+    'func', [
+        two_sum,
+        two_sum_reverse,
+        with_buffer
+    ]
+)
+@pytest.mark.parametrize(
+    ('nums', 'target', 'indexes'), (
         ([2, 7, 11, 15], 9, [0, 1]),
         ([3, 2, 4], 6, [1, 2]),
         ([3, 3], 6, [0, 1]),
         ([-1, 2, -3, -5, 6, -7], -10, [2, 5]),
         ([0, 0], 0, [0, 1])
-    ]
+    )
 )
-def test(nums, target, indexes):
-    assert two_sum(nums, target) == indexes
+def test(func, nums, target, indexes):
+    assert func(nums, target) == indexes
