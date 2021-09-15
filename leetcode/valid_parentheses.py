4a52267c2a6e5116f27b5afcd06b43116055bb05
netotz <neto.otz@hotmail.com>
2021-09-15 15:32:03 -0500

    Solve Valid Parentheses using a stack

---
+++ leetcode/valid_parentheses.py
@@ -0,0 +1,35 @@
+import pytest
+
+
+def is_valid(string: str) -> bool:
+    parentheses = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+
+    stack = list()
+    for char in string:
+        if char in parentheses.keys():
+            stack.append(char)
+        else:
+            last_opened = stack.pop()
+            if parentheses[last_opened] != char:
+                return False
+    return True
+
+
+@pytest.mark.parametrize(
+    ('string', 'is_it'), (
+        ('()', True),
+        ('()[]{}', True),
+        ('(]', False),
+        ('([)]', False),
+        ('{[]}', True),
+        ('([[]()]{{}{()}})', True),
+        ('([]{{([][](())}[])}})', False)
+    )
+)
+def test(string, is_it):
+    assert is_valid(string) == is_it
+
