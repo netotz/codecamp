e9f7d7951c874d5bc765b2f58d17bb1cb061247b
netotz <neto.otz@hotmail.com>
2021-09-15 13:38:52 -0500

    Add solution using a deque

--- kattis/intlists.py
+++ kattis/intlists.py
@@ -1,4 +1,5 @@
 from typing import List, Union
+from collections import deque
 
 import pytest
 
@@ -7,6 +8,7 @@ def run_bapc(bapc: str, intslist: List[int]) -> Union[List[int], str]:
     R = 'R'
     D = 'D'
 
+    newlist = list(intslist)
     same = 1
     i = 0
     while i < len(bapc):
@@ -15,26 +17,55 @@ def run_bapc(bapc: str, intslist: List[int]) -> Union[List[int], str]:
         if current != next or next == '':
             if current == R:
                 if same % 2 == 1:
-                    intslist.reverse()
+                    newlist.reverse()
             elif current == D:
-                if same > len(intslist):
+                if same > len(newlist):
                     return 'error'
-                intslist = intslist[same:]
+                newlist = newlist[same:]
             same = 1
         else:
             same += 1
         i += 1
-    return intslist
+    return newlist
 
 
+def run_bapc_deque(bapc: str, intslist: List[int]) -> Union[List[int], str]:
+    R = 'R'
+    D = 'D'
+
+    deq = deque(intslist)
+    pophead = True
+    for func in bapc:
+        if func == R:
+            pophead = not pophead
+        else:
+            if not deq:
+                return 'error'
+            if pophead:
+                deq.popleft()
+            else:
+                deq.pop()
+    if not pophead:
+        deq.reverse()
+    return list(deq)
+
+
+@pytest.mark.parametrize(
+    'func', (
+        run_bapc,
+        run_bapc_deque
+    )
+)
 @pytest.mark.parametrize(
     ('bapc', 'intslist', 'result'), (
         ('RDD', [1, 2, 3, 4], [2, 1]),
         ('DD', [42], 'error'),
-        ('RRD', [1,1,2,3,5,8], [1,2,3,5,8]),
+        ('RRD', [1, 1, 2, 3, 5, 8], [1, 2, 3, 5, 8]),
         ('D', [], 'error'),
-        ('DRRRRRD', [4, 8, 10], [8])
+        ('DRRRRRD', [4, 8, 9, 10], [9, 8]),
+        ('RDRD', [1, 2, 3, 4, 5], [2, 3, 4]),
+        ('DDRDRDRRD', [2, 3, 5, 7, 11, 13, 17, 23], [11, 13, 17])
     )
 )
-def test(bapc, intslist, result):
-    assert run_bapc(bapc, intslist) == result
+def test(func, bapc, intslist, result):
+    assert func(bapc, intslist) == result
