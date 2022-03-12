e5ab5d2e11654d09fc4b914b1ccaf985dd062f88
netotz <neto.otz@hotmail.com>
2022-03-12 16:10:49 -0600

    Add description of puzzle

--- meta/preparation/cipher.py
+++ meta/preparation/cipher.py
@@ -1,3 +1,21 @@
+'''
+Rotational Cipher
+One simple way to encrypt a string is to "rotate" every alphanumeric character by a certain amount. Rotating a character means replacing it with another character that is a certain number of steps away in normal alphabetic or numerical order.
+For example, if the string "Zebra-493?" is rotated 3 places, the resulting string is "Cheud-726?". Every alphabetic character is replaced with the character 3 letters higher (wrapping around from Z to A), and every numeric character replaced with the character 3 digits higher (wrapping around from 9 to 0). Note that the non-alphanumeric characters remain unchanged.
+Given a string and a rotation factor, return an encrypted string.
+
+Input
+1 <= |input| <= 1,000,000
+0 <= rotationFactor <= 1,000,000
+
+Output
+Return the result of rotating input a number of times equal to rotationFactor.
+'''
+
+
+import pytest
+
+
 def rotate_char(char_code: int, set_min: int, set_max: int, rotation_factor: int) -> str:
     '''
     Rotates an ASCII character given the set it belongs to (lowercase, uppercase, or digits).
@@ -62,48 +80,6 @@ def rotationalCipher(input: str, rotation_factor: int) -> str:
         output.append(rotated_char)
 
     return ''.join(output)
-
-
-# These are the tests we use to determine if the solution is correct.
-# You can add your own at the bottom.
-
-def printString(string):
-  print('[\"', string, '\"]', sep='', end='')
-
-test_case_number = 1
-
-def check(expected, output):
-  global test_case_number
-  result = False
-  if expected == output:
-    result = True
-  rightTick = '\u2713'
-  wrongTick = '\u2717'
-  if result:
-    print(rightTick, 'Test #', test_case_number, sep='')
-  else:
-    print(wrongTick, 'Test #', test_case_number, ': Expected ', sep='', end='')
-    printString(expected)
-    print(' Your output: ', end='')
-    printString(output)
-    print()
-  test_case_number += 1
-
-if __name__ == "__main__":
-  input_1 = "All-convoYs-9-be:Alert1."
-  rotation_factor_1 = 4
-  expected_1 = "Epp-gsrzsCw-3-fi:Epivx5."
-  output_1 = rotationalCipher(input_1, rotation_factor_1)
-  check(expected_1, output_1)
-
-  input_2 = "abcdZXYzxy-999.@"
-  rotation_factor_2 = 200
-  expected_2 = "stuvRPQrpq-999.@"
-  output_2 = rotationalCipher(input_2, rotation_factor_2)
-  check(expected_2, output_2)
-
-
-import pytest
 
 
 @pytest.mark.parametrize(
