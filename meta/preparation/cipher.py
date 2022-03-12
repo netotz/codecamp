2d831dfd894e023cdef80fe623bc511ba38278a7
netotz <neto.otz@hotmail.com>
2022-03-12 11:28:07 -0600

    Improve solution of cipher

--- meta/preparation/cipher.py
+++ meta/preparation/cipher.py
@@ -1,40 +1,67 @@
-import string
-import pytest
+def rotate_char(char_code: int, set_min: int, set_max: int, rotation_factor: int) -> str:
+    '''
+    Rotates an ASCII character given the set it belongs to (lowercase, uppercase, or digits).
 
-
-def get_rotated_char(original_char: str, collection: str, rotation_factor: int) -> str:
-    # O(c)
-    position = collection.find(original_char)
-    rotated_position = (position + rotation_factor) % len(collection)
-    return collection[rotated_position]
+    O(1)
+    '''
+    # code of character within its set
+    set_code = char_code - set_min
+    # lenght of set is the difference of max and min indexes, plus 1
+    # because first index is 0 !!
+    set_length = abs(set_max - set_min) + 1
+    # rotated code of char but within its set
+    set_rotated = (set_code + rotation_factor) % set_length
+    # rotated code of char in ASCII !!
+    rotated_code = set_rotated + set_min
+    return chr(rotated_code)
 
 
 def rotationalCipher(input: str, rotation_factor: int) -> str:
-    output = ''
+    '''
+    O(n)
+    '''
+    # use list as a string builder
+    output = list()
+    # use dictionary to store ciphered chars and not having to rotate them again
     rotated_chars = dict()
-    # O(nc)
+
+    # O(n)
     for character in input:
         if character in rotated_chars:
-            output += rotated_chars[character]
+            output.append(rotated_chars[character])
             continue
 
-        collection = ''
-        if character in string.ascii_lowercase:
-            collection = string.ascii_lowercase
-        elif character in string.ascii_uppercase:
-            collection = string.ascii_uppercase
-        elif character in string.digits:
-            collection = string.digits
-        # character is not alphanumeric
+        char_code = ord(character)
+
+        UPPERCASE_MIN = ord('A')
+        UPPERCASE_MAX = ord('Z')
+
+        LOWERCASE_MIN = ord('a')
+        LOWERCASE_MAX = ord('z')
+
+        DIGIT_MIN = ord('0')
+        DIGIT_MAX = ord('9')
+
+        set_min = 0
+        set_max = 0
+        if UPPERCASE_MIN <= char_code <= UPPERCASE_MAX:
+            set_min = UPPERCASE_MIN
+            set_max = UPPERCASE_MAX
+        elif LOWERCASE_MIN <= char_code <= LOWERCASE_MAX:
+            set_min = LOWERCASE_MIN
+            set_max = LOWERCASE_MAX
+        elif DIGIT_MIN <= char_code <= DIGIT_MAX:
+            set_min = DIGIT_MIN
+            set_max = DIGIT_MAX
         else:
-            output += character
+            output.append(character)
             continue
 
-        rotated_char = get_rotated_char(character, collection, rotation_factor)
+        rotated_char = rotate_char(char_code, set_min, set_max, rotation_factor)
         rotated_chars[character] = rotated_char
-        output += rotated_char
+        output.append(rotated_char)
 
-    return output
+    return ''.join(output)
 
 
 # These are the tests we use to determine if the solution is correct.
@@ -74,6 +101,10 @@ if __name__ == "__main__":
   expected_2 = "stuvRPQrpq-999.@"
   output_2 = rotationalCipher(input_2, rotation_factor_2)
   check(expected_2, output_2)
+
+
+import pytest
+
 
 @pytest.mark.parametrize(
     ('original', 'rotation', 'encrypted'), (
