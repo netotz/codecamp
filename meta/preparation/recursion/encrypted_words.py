2d34fedc99446d93cdf3750ae3c10e55901a4a9f
netotz <neto.otz@hotmail.com>
2022-03-16 23:26:03 -0600

    meta: add coments

--- meta/preparation/recursion/encrypted_words.py
+++ meta/preparation/recursion/encrypted_words.py
@@ -2,9 +2,10 @@
 Encrypted Words
 You've devised a simple encryption method for alphabetic strings that shuffles the characters in such a way that the resulting string is hard to quickly read, but is easy to convert back into the original string.
 When you encrypt a string S, you start with an initially-empty resulting string R and append characters to it as follows:
-Append the middle character of S (if S has even length, then we define the middle character as the left-most of the two central characters)
-Append the encrypted version of the substring of S that's to the left of the middle character (if non-empty)
-Append the encrypted version of the substring of S that's to the right of the middle character (if non-empty)
+1. Append the middle character of S (if S has even length, then we define the middle character as the left-most of the two central characters).
+2. Append the encrypted version of the substring of S that's to the left of the middle character (if non-empty).
+3. Append the encrypted version of the substring of S that's to the right of the middle character (if non-empty).
+
 For example, to encrypt the string "abc", we first take "b", and then append the encrypted version of "a" (which is just "a") and the encrypted version of "c" (which is just "c") to get "bac".
 If we encrypt "abcxcba" we'll get "xbacbca". That is, we take "x" and then append the encrypted version "abc" and then append the encrypted version of "cba".
 
@@ -38,10 +39,12 @@ import pytest
 
 def encrypt(string: str, start: int, end: int) -> list[str]:
     '''
-    Encrypts the substring of `string` between indexes `start` and `end`,
+    Recursively encrypts the substring of `string` between indexes `start` and `end`,
     `substring = string[start:end + 1]`.
 
-    O(n), not sure how
+    O(n) time, because every character is visited once
+
+    O(log n) space, as at the same time there will be log n call stacks
     '''
     # if starting index is greater than ending,
     # substring is an empty string,
@@ -69,8 +72,14 @@ def encrypt(string: str, start: int, end: int) -> list[str]:
     return encrypted
 
 
-def findEncryptedWord(s: str) -> str:
-    return ''.join(encrypt(s, 0, len(s) - 1))
+def findEncryptedWord(string: str) -> str:
+    # first index of string, 0
+    first = 0
+    # last index of string
+    last = len(string) - 1
+    # encrypt the "substring" of string from first index to last index,
+    # in this case because it's the first call, it's the whole string
+    return ''.join(encrypt(string, first, last))
 
 
 @pytest.mark.parametrize(
