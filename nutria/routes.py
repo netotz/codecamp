da3dd65956957f42db4510ed17e66ccb5aa34be7
netotz <neto.otz@hotmail.com>
2022-03-23 15:15:13 -0600

    nutra: improve solution

--- nutria/routes.py
+++ nutria/routes.py
@@ -12,52 +12,53 @@ human/paul
 provide the best data structure to parse and store these routes.
 '''
 
-# TODO: review solution
-
-
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class TreeNode:
     label: str
-    children: list['TreeNode'] = None
+    children: dict[str, 'TreeNode'] = field(init=False, default_factory=dict)
 
-roots = []
 
-def add_node(node: TreeNode, labels: list[str]) -> None:
-    if len(labels) == 0:
+def add_node(node: TreeNode, labels: list[str], index: int) -> None:
+    if index == len(labels):
         return
     
-    current_label = labels[0]
-    # was_found = False
-    # O(c) -> O(1)
-    for child in node.children:
-        if child.label == current_label:
-            # was_found = True
-            add_node(child, labels[1:])
+    current_label = labels[index]
 
-    # if not was_found:
-    new_child = TreeNode(current_label)
-    node.children.append(new_child)
-    add_node(new_child, labels[1:])
+    child = None
+    if current_label in node.children:
+        child = node.children[current_label]
+    else:
+        child = TreeNode(current_label)
+        node.children[current_label] = child
+
+    add_node(child, labels, index + 1)
 
 
-def build_tree(route: str) -> None:
-    routes = route.split('/')
-    current_node = None
-    for root in roots:
-        if root.label == routes[0]:
-            current_node = root
-    if current_node is None:
-        current_node = TreeNode(routes[0])
-        roots.append(current_node)
-
-    labels = routes[1:]
-    add_node(current_node, labels)
+def build_trees(routes: list[str]) -> TreeNode:
+    '''
+    Time O(n), where n is the total number of labels.
+    '''
+    trees = TreeNode('')
     
-    # for r in routes:
-        
-# list of n strings
-# each string is a route, r labels
-# O(nrc)
+    # O(r)
+    for route in routes:
+        labels = route.split('/')
+        # O(l)
+        add_node(trees, labels, 0)
+    
+    return trees
+
+
+# each row is a route, each word is a label
+inputs = [
+    'animal/reptile/lizard',
+    'plant/tree/cherry',
+    'animal/feline/tiger',
+    'animal/canine/wolf',
+    'animal/lizard',
+    'plant/tree/pine',
+    'human/paul',
+]
