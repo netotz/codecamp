d52af7971651e2c88be81c8f9a47a3f45e7d5b0a
netotz <neto.otz@hotmail.com>
2022-05-03 21:39:31 -0500

    gfg: add Union Find solution

--- geeksforgeeks/number_unconnected_components.py
+++ geeksforgeeks/number_unconnected_components.py
@@ -11,6 +11,58 @@ from collections import deque
 import pytest
 
 
+def unionfind(nodes: int, edges: list[list[int]]) -> int:
+    """
+    time O(e + log n)?
+    """
+
+    def find(v: int) -> int:
+        curr = v
+
+        # while current it's not parent of itself
+        while curr != parents[curr]:
+            grandpa = parents[parents[curr]]
+            # set grandpa of current to be its new parent
+            # path compression
+            parents[curr] = grandpa
+
+            curr = parents[curr]
+
+        return curr
+
+    def try_union(u: int, v: int) -> bool:
+        # find parents
+        up = find(u)
+        vp = find(v)
+
+        # if both nodes are in same group
+        if up == vp:
+            return False
+
+        # add shorter group as child of bigger group
+        if sizes[up] > sizes[vp]:
+            parents[vp] = up
+            sizes[up] += sizes[vp]
+        else:
+            parents[up] = vp
+            sizes[vp] += sizes[up]
+
+        return True
+
+    # O(n)
+    parents = [v for v in range(nodes)]
+    # O(n)
+    sizes = [1] * nodes
+
+    count = nodes
+    # O(e)
+    for u, v in edges:
+        if try_union(u, v):
+            count -= 1
+
+    return count
+
+
 def count_components(nodes: int, edges: list[list[int]]) -> int:
     """
     time O(n + e)
@@ -51,6 +103,7 @@ def count_components(nodes: int, edges: list[list[int]]) -> int:
     return count
 
 
+@pytest.mark.parametrize("function", [count_components, unionfind])
 @pytest.mark.parametrize(
     ("nodes", "edges", "count"),
     [
@@ -58,5 +111,5 @@ def count_components(nodes: int, edges: list[list[int]]) -> int:
         (8, [[1, 0], [0, 2], [3, 5], [3, 4], [6, 7]], 3),
     ],
 )
-def test(nodes, edges, count):
-    assert count_components(nodes, edges) == count
+def test(function, nodes, edges, count):
+    assert function(nodes, edges) == count
