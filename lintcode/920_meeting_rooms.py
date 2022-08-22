16a1258a9ac372fc760a732e4b898c08ef520763
netotz <neto.otz@hotmail.com>
2022-08-22 16:51:10 -0500

    lintcode: fix condition

--- lintcode/920_meeting_rooms.py
+++ lintcode/920_meeting_rooms.py
@@ -38,7 +38,7 @@ def can_attend(intervals: list[Interval]) -> bool:
 
     # O(n)
     for i in range(1, len(intervals)):
-        if intervals[i].start <= intervals[i - 1].end:
+        if intervals[i].start < intervals[i - 1].end:
             return False
 
     return True
